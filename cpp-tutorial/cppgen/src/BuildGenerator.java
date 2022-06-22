c256b3aa10f1ef1e3ab12db6431744cbebe64f4f
Orlando Ding <xiandao.airs@gmail.com>
2022-06-22 09:05:06 +0800

    orlando - for updates of implementatino

--- cpp-tutorial/cppgen/src/BuildGenerator.java
+++ cpp-tutorial/cppgen/src/BuildGenerator.java
@@ -2,9 +2,6 @@ import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -17,9 +14,9 @@ public class BuildGenerator {
         velocityEngine.init();
         Template cc = velocityEngine.getTemplate("BUILD.vm");
         VelocityContext context = new VelocityContext();
-        context.put("ns1", IntStream.range(1, 1001).boxed().collect(Collectors.toList()));
-        context.put("ns2", IntStream.range(1001, 2001).boxed().collect(Collectors.toList()));
-        context.put("ns3", IntStream.range(2001, 3001).boxed().collect(Collectors.toList()));
+        context.put("ns1", IntStream.range(1, 1501).boxed().collect(Collectors.toList()));
+        context.put("ns2", IntStream.range(1501, 3001).boxed().collect(Collectors.toList()));
+        context.put("ns3", IntStream.range(3001, 4501).boxed().collect(Collectors.toList()));
         try(Writer writer = new FileWriter("BUILD")) {
             cc.merge(context, writer);
         }
